cb469ffbd6cf8e973ea18e6b3fef365911fd6afc
Lucas <kucasss19@gmail.com>
2025-09-26 07:36:08 -0300

    feat: refatoração do modulo[

--- simulation/qsim/testeFila.do
+++ simulation/qsim/testeFila.do
@@ -1,11 +1,11 @@
 onerror {exit -code 1}
 vlib work
 vlog -work work testeFila.vo
-vlog -work work Waveform.vwf.vt
-vsim -c -t 1ps -L cyclonev_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate_ver -L altera_lnsim_ver work.control_vlg_vec_tst
+vlog -work work Waveform3.vwf.vt
+vsim -c -t 1ps -L cyclonev_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate_ver -L altera_lnsim_ver work.unidade_de_controle_vlg_vec_tst
 vcd file -direction testeFila.msim.vcd
-vcd add -internal control_vlg_vec_tst/*
-vcd add -internal control_vlg_vec_tst/i1/*
+vcd add -internal unidade_de_controle_vlg_vec_tst/*
+vcd add -internal unidade_de_controle_vlg_vec_tst/i1/*
 proc simTimestamp {} {
     echo "Simulation time: $::now ps"
     if { [string equal running [runStatus]] } {
@@ -15,13 +15,3 @@ proc simTimestamp {} {
 after 2500 simTimestamp
 run -all
 quit -f
-
-
-
-
-
-
-
-
-
-
--- unidade_de_controle.v
+++ unidade_de_controle.v
@@ -3,7 +3,6 @@
 		input botao_zoom_in,
 		input botao_zoom_out,
 		input seletor_algoritmo,
-		output [7:0] dados,
 		output wire hsync,
 		output wire vsync,    
 		output [7:0] red,     
@@ -25,32 +24,19 @@
 		 );
 
 
-		reg [2:0] estado_atual;
-		reg [2:0] proximo_estado;
-		reg [16:0] endereco_memoria;
-		
-		reg [16:0] endereco_escrita;
-		reg permicao_escrita;
-		
-		reg escrita_dados;
-		
-		reg botao_zoom_in_reg;
-		reg botao_zoom_out_reg;
-		reg seletor_algoritmo_reg;
-		
-		reg [7:0] pixel_para_processar_reg;
-		wire [7:0] pixel_para_processar;
-		
-		wire [31:0] pixels_processados;
-		
-		reg pixel_para_salvar;
-		reg [31:0] pixels_processados_reg;
-
-		reg [1:0] salvar_pixels;
-		
-		reg dados_prontos;
-		
-		reg [2:0] opcode;
+		 reg [2:0] estado_atual, proximo_estado;
+		 reg [16:0] endereco_memoria, endereco_memoria_next;
+		 reg [16:0] endereco_escrita, endereco_escrita_next;
+		 reg escrita_dados, escrita_dados_next;
+		 reg botao_zoom_in_reg, botao_zoom_out_reg, seletor_algoritmo_reg;
+		 reg [7:0] pixel_para_processar_reg;
+		 reg [31:0] pixels_processados_reg;
+		 reg [7:0] pixel_para_salvar, pixel_para_salvar_next;
+		 reg [1:0] salvar_pixels, salvar_pixels_next;
+		 reg dados_prontos, dados_prontos_next;
+		 reg [2:0] opcode, opcode_next;
+		 wire [7:0] pixel_para_processar;
+		 wire [31:0] pixels_processados;
 		
 		parameter ENDERECO_BASE = 17'd38560;
 		
@@ -58,6 +44,135 @@
 		
 		parameter ENDERECO_1=0,ENDERECO_2=1, ENDERECO_3=2,ENDERECO_4=3;
 		
+		always @(posedge clock) begin
+        estado_atual <= proximo_estado;
+		  
+		  
+		  if (estado_atual == IDLE) begin
+			  botao_zoom_in_reg <= ~botao_zoom_in;
+			  botao_zoom_out_reg <= ~botao_zoom_out;
+			  seletor_algoritmo_reg <= seletor_algoritmo;
+		  end else begin
+			  botao_zoom_in_reg <= botao_zoom_in_reg;
+			  botao_zoom_out_reg <= botao_zoom_out_reg;
+			  seletor_algoritmo_reg <= seletor_algoritmo_reg;
+		  end
+        
+        // Registrar próximos valores
+        endereco_memoria <= endereco_memoria_next;
+        endereco_escrita <= endereco_escrita_next;
+        escrita_dados <= escrita_dados_next;
+        pixel_para_salvar <= pixel_para_salvar_next;
+        salvar_pixels <= salvar_pixels_next;
+        dados_prontos <= dados_prontos_next;
+        opcode <= opcode_next;
+        pixels_processados_reg <= pixels_processados; // Registrar saída da ALU
+        pixel_para_processar_reg <= pixel_para_processar; // Registrar leitura RAM
+    end
+
+    // BLOCO COMBINACIONAL - apenas atribuições com =
+    always @(*) begin
+        // Valores padrão (evitar latches)
+        proximo_estado = estado_atual;
+        endereco_memoria_next = endereco_memoria;
+        endereco_escrita_next = endereco_escrita;
+        escrita_dados_next = escrita_dados;
+        pixel_para_salvar_next = pixel_para_salvar;
+        salvar_pixels_next = salvar_pixels;
+        dados_prontos_next = dados_prontos;
+        opcode_next = opcode;
+
+        case (estado_atual)
+            IDLE: begin
+                escrita_dados_next = 1'b0;
+                endereco_memoria_next = ENDERECO_BASE;
+                dados_prontos_next = 1'b0;
+                
+                if (botao_zoom_in_reg || botao_zoom_out_reg) begin
+                    proximo_estado = LOAD_OP;
+                end else begin
+                    proximo_estado = IDLE;
+                end
+            end
+            
+            LOAD_OP: begin
+                opcode_next = {botao_zoom_in_reg, botao_zoom_out_reg, seletor_algoritmo_reg};
+                proximo_estado = READ_PIXEL;
+            end
+            
+            READ_PIXEL: begin
+                // pixel_para_processar_reg é registrado no bloco sequencial
+                proximo_estado = EXECUTE;
+            end
+            
+            EXECUTE: begin
+                salvar_pixels_next = ENDERECO_1;
+                proximo_estado = WRITE;
+            end
+            
+            WRITE: begin
+                escrita_dados_next = 1'b1;
+                
+                case (salvar_pixels)
+                    ENDERECO_1: begin
+                        endereco_escrita_next = endereco_memoria - ENDERECO_BASE;
+                        pixel_para_salvar_next = pixels_processados[7:0]; // Usar saída direta da ALU
+                        salvar_pixels_next = ENDERECO_2;
+                        proximo_estado = WRITE;
+                    end
+                    ENDERECO_2: begin
+                        endereco_escrita_next = (endereco_memoria + 1'b1) - ENDERECO_BASE;
+                        pixel_para_salvar_next = pixels_processados[15:8];
+                        salvar_pixels_next = ENDERECO_3;
+                        proximo_estado = WRITE;
+                    end
+                    ENDERECO_3: begin
+                        endereco_escrita_next = (endereco_memoria + 9'd320) - ENDERECO_BASE;
+                        pixel_para_salvar_next = pixels_processados[23:16];
+                        salvar_pixels_next = ENDERECO_4;
+                        proximo_estado = WRITE;
+                    end
+                    ENDERECO_4: begin
+                        endereco_escrita_next = (endereco_memoria + 9'd321) - ENDERECO_BASE;
+                        pixel_para_salvar_next = pixels_processados[31:24];
+                        escrita_dados_next = 1'b0;
+                        salvar_pixels_next = ENDERECO_1;
+                        
+                        if (endereco_escrita_next >= 17'd76800) begin
+                            proximo_estado = END_INSTRUCTION;
+                        end else begin
+                            proximo_estado = NEXT_PIXEL;
+                        end
+                    end
+                    default: begin
+                        proximo_estado = IDLE;
+                    end
+                endcase
+            end
+            
+            NEXT_PIXEL: begin
+                endereco_memoria_next = endereco_memoria + 1'b1;
+                
+                if (endereco_memoria_next >= 17'd76800) begin
+                    proximo_estado = END_INSTRUCTION;
+                end else begin
+                    proximo_estado = LOAD_OP;
+                end
+            end
+            
+            END_INSTRUCTION: begin
+                dados_prontos_next = 1'b1;
+                proximo_estado = IDLE;
+            end
+            
+            default: begin
+                proximo_estado = IDLE;
+            end
+        endcase
+    end
+		
+		
+		/*
 		always @(posedge clock)
 		 begin
 					estado_atual <= proximo_estado;
@@ -164,7 +279,7 @@
 				
 				endcase	  
 		  end
-
+*/
 
 	ram_primaria ram_leitura(
 		.address(endereco_memoria),
